747b607b97b851964675e5eb7451003227b55db6
meidencore <danieleverdale@gmail.com>
2024-12-28 17:10:27 -0500

    initial commit

---
+++ chapter2/input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  int number = 0;
+  printf("%s", "enter a number: ");
+  scanf("%d", &number);
+  printf("the number is : %d", number);
+  return 0;
+}
---
+++ chapter2/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int number1 = 0;
+    int number2 = 0;
+
+    printf("Enter firts number: ");
+    scanf("%d", &number1);
+
+    printf("Enter second number: ");
+    scanf("%d", &number2);
+
+    printf("The sum is equal %d", number1 + number2);
+}
+
